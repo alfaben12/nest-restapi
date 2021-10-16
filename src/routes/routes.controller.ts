aaf88d602716481db38a45d1dac1b432fb2cc273
Alfaben <zrav420@gmail.com>
2021-10-16 22:31:56 +0700

    update routes

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Request } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { Request as ExpressRequest, Router } from 'express';
 import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
 
 @Controller({ path: 'routes', version: '1' })
 export class RoutesController {
@@ -10,24 +11,22 @@ export class RoutesController {
   @Post('sync')
   async sync(@Request() req: ExpressRequest) {
     const router = req.app._router as Router;
-    const routes: any = [];
 
-    const alreadySavedRoutes = await this.routesService.findAll();
+    let countNewRoute: number = 0;
+    let newRouteArray: string[] = [];
+
+    // NON ACTIVE ALLL ROUTES
+    await this.routesService.updateNonActiveAll();
     router.stack.map(async (layer) => {
       if (layer.route) {
         const path = layer.route?.path;
         const method = layer.route?.stack[0].method;
-        // const checkAlreadyExist = await this.routesService.findOne(
-        //   path,
-        //   method,
-        // );
+        const checkAlreadyExist: boolean = await this.routesService.findOne(
+          path,
+          method,
+        ) ? true : false;
 
-        // if (checkAlreadyExist) {
-        //   const updateRouteDto: UpdateRouteDto = new UpdateRouteDto();
-        //   updateRouteDto.isActive = false;
-
-        //   await this.routesService.updateActive(path, updateRouteDto);
-        // } else {
+        if (!checkAlreadyExist) {
           const createRouteDto: CreateRouteDto = new CreateRouteDto();
           createRouteDto.path = path;
           createRouteDto.method = method;
@@ -35,20 +34,21 @@ export class RoutesController {
           createRouteDto.role = ['ADMIN'];
 
           this.routesService.create(createRouteDto);
-        // }
+          newRouteArray.push(`${path} - ${method}`)
+          countNewRoute++
+        }
 
-        routes.push({
-          path,
-          method,
-        });
+        const updateRouteDto: UpdateRouteDto = new UpdateRouteDto();
+        updateRouteDto.isActive = true;
+
+        // UPDATE ACTIVE ROUTE
+        await this.routesService.updateActive(path, method, updateRouteDto);
       }
     });
 
-    const results = routes.filter(
-      ({ path: id1 }) =>
-        !alreadySavedRoutes.some(({ path: id2 }) => id2 === id1),
-    );
-
-    return alreadySavedRoutes;
+    return {
+      countNewRoute,
+      newRouteArray
+    };
   }
 }
--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { getConnection, getManager, getRepository, Repository } from 'typeorm';
-import { CreateRouteDto } from './dto/create-route.dto';
-import { UpdateRouteDto } from './dto/update-route.dto';
-import { Route } from './entities/route.entity';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { getConnection, getManager, getRepository, Repository } from "typeorm";
+import { CreateRouteDto } from "./dto/create-route.dto";
+import { UpdateRouteDto } from "./dto/update-route.dto";
+import { Route } from "./entities/route.entity";
 
 @Injectable()
 export class RoutesService {
   constructor(
     @InjectRepository(Route)
-    private routesRepository: Repository<Route>,
+    private routesRepository: Repository<Route>
   ) {}
 
   findAll() {
-    return this.routesRepository.find({ select: ['path', 'method'] });
+    return this.routesRepository.find({ select: ["path", "method"] });
   }
 
   create(createRouteDto: CreateRouteDto) {
@@ -24,11 +24,22 @@ export class RoutesService {
     return this.routesRepository.findOne({ path, method });
   }
 
+  async updateNonActiveAll(): Promise<Boolean> {
+    return (await getConnection()
+      .createQueryBuilder()
+      .update(Route)
+      .set({ isActive: false })
+      .execute())
+      ? true
+      : false;
+  }
+
   async updateActive(
     path: string,
-    updateRouteDto: UpdateRouteDto,
+    method: string,
+    updateRouteDto: UpdateRouteDto
   ): Promise<Route> {
-    await this.routesRepository.update({ path }, updateRouteDto);
-    return this.routesRepository.findOne({ path });
+    await this.routesRepository.update({ path, method }, updateRouteDto);
+    return this.routesRepository.findOne({ path, method });
   }
 }
